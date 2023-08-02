c24da903a15c7d2cb0da7609c795ed718a0bb2d3
unknown <marlon_tancredo@hotmail.com>
2023-08-02 20:50:37 +0100

    refactor: refactoring moveRover.test.ts

--- test/moveRover.test.ts
+++ test/moveRover.test.ts
@@ -4,15 +4,15 @@ import { mapArea } from "../src/mars/terrain";
 describe("moveRover()", () => {
   const map = mapArea(5, 5);
 
-  it("should return the rover position", () => {
-    expect(moveRover("LMLMLMLMM", map)).toBe("1 3 north");
+  it("should return 1 3 N", () => {
+    expect(moveRover("LMLMLMLMM", map)).toBe("1 3 N");
   });
 
-  it("should return position to west", () => {
-    expect(moveRover("L", map)).toBe("1 3 west");
+  it("should return 1 3 W", () => {
+    expect(moveRover("L", map)).toBe("1 3 W");
   });
 
-  it("should return position to east", () => {
-    expect(moveRover("RR", map)).toBe("1 3 east");
+  it("should return 1 2 E", () => {
+    expect(moveRover("RR", map)).toBe("1 3 E");
   });
 });
