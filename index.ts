96fc90bc81efe29734474a46b481695d1e7bf703
unknown <marlon_tancredo@hotmail.com>
2023-08-02 20:47:57 +0100

    refactor: refactoring index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import { mapArea } from "./src/mars/terrain";
 import { landRover, moveRover } from "./src/vehicles/rover/rover";
+import { displayRoverPosition } from "./src/vehicles/parts/screen";
 
 //set terrain size 🗺️
 const mappedArea = mapArea(5, 5);
 
 //Initial position 🪂
-console.log(landRover(mappedArea));
+landRover(mappedArea);
+console.log(displayRoverPosition(mappedArea));
 
 //Moving rover 🕹️
-console.log(moveRover("LMLMLMLMM", mappedArea));
+moveRover("L", mappedArea);
+console.log(displayRoverPosition(mappedArea));
