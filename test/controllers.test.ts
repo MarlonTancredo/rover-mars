843c6cb17f4c3ad7419bd748d32ed91ac8390626
unknown <marlon_tancredo@hotmail.com>
2023-08-04 23:10:24 +0100

    refact: refactoring controllers.test.ts

--- test/controllers.test.ts
+++ test/controllers.test.ts
@@ -1,54 +1,62 @@
 import { mapArea } from "../src/mars/terrain";
-import { getMap } from "../src/vehicles/rover/rover";
-import { landingRover, movingRover } from "../src/vehicles/rover/controllers";
+import { getMap } from "../src/vehicles/roverA/rover";
+import { landingRover, movingRover } from "../src/vehicles/roverA/controllers";
 
 describe("landingRover() ", () => {
+  //Arrange
+  const map = mapArea(5, 5);
+  const x = 1;
+  const y = 2;
+  const direction = "N";
+
   it("should return 1 2 N", () => {
-    const map = mapArea(5, 5);
-    const x = 1;
-    const y = 2;
-    const direction = "N";
-
+    //Act
     getMap(map);
-
+    //Assert
     expect(landingRover(x, y, direction)).toBe("1 2 N");
   });
 });
 
 describe("movingRover() calling many movingRover()", () => {
+  //Arrange
+  const map = mapArea(5, 5);
+  const x = 1;
+  const y = 2;
+  const direction = "N";
+
   it("should return 1 3 N", () => {
-    const map = mapArea(5, 5);
-    const x = 1;
-    const y = 2;
-    const direction = "N";
-
+    //Act
     getMap(map);
-
     landingRover(x, y, direction);
 
+    //Assert
     expect(movingRover("LMLMLMLMM")).toBe("1 3 N");
   });
 
+  //Arrange Act Assert
   it("should return 1 2 W", () => {
     expect(movingRover("L")).toBe("1 3 W");
   });
 
+  //Arrange Act Assert
   it("should return 1 4 W", () => {
     expect(movingRover("M")).toBe("1 4 W");
   });
 });
 
 describe("movingRover() calling one movingRover()", () => {
+  //Arrange
+  const map = mapArea(5, 5);
+  const x = 3;
+  const y = 3;
+  const direction = "E";
+
   it("should return 5 1 E", () => {
-    const map = mapArea(5, 5);
-    const x = 3;
-    const y = 3;
-    const direction = "E";
-
+    //Act
     getMap(map);
-
     landingRover(x, y, direction);
 
+    //Assert
     expect(movingRover("MMRMMRMRRM")).toBe("5 1 E");
   });
 });
